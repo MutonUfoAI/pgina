0ec7d82b263257abb23a2ba96647e35d720a7208
Patrick Gill <git@inishowenlabs.com>
2018-05-23 20:27:02 +0000

    Updated version number

--- pGina/src/Core/Properties/SharedAssemblyInfo.cs
+++ pGina/src/Core/Properties/SharedAssemblyInfo.cs
@@ -17,5 +17,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.9.9.12")]
-[assembly: AssemblyFileVersion("3.9.9.12")]
+[assembly: AssemblyVersion("3.9.9.13")]
+[assembly: AssemblyFileVersion("3.9.9.13")]
